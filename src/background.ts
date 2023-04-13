79a59d6340e47646cb107e27ede2153fb06a2d44
kawamataryo <ba068082@gmail.com>
2023-04-13 13:39:59 +0900

    refactor: remove unused aync

--- src/background.ts
+++ src/background.ts
@@ -2,7 +2,7 @@ import { sendToContentScript } from "@plasmohq/messaging"
 import { INITIALIZE_SUBMIT_BLOCK_MESSAGE_NAME } from "~lib/constants"
 
 // SPAでのページ遷移への対応
-(async () => {
+(() => {
   chrome.history.onVisited.addListener(
     async () => {
       await sendToContentScript({
