b1dbe40cc6663fa03677a4e8f00d97d6723b776c
kawamataryo <ba068082@gmail.com>
2023-04-18 08:27:07 +0900

    fix: change listner to dom

--- src/lib/preventEnterOnIME.ts
+++ src/lib/preventEnterOnIME.ts
@@ -1,42 +1,52 @@
-export const preventEnterOnIME = (dom: Document | ShadowRoot, signal: AbortSignal) => {
-  const webComponents = Array.from(dom.querySelectorAll('*')).filter((d) => d.tagName.includes('-'));
+export const preventEnterOnIME = (
+  dom: Document | ShadowRoot,
+  signal: AbortSignal
+) => {
+  const webComponents = Array.from(dom.querySelectorAll("*")).filter((d) =>
+    d.tagName.includes("-")
+  )
 
-  let lastKeyDownIsIME = false;
+  let lastKeyDownIsIME = false
 
-  webComponents.forEach((d) =>  {
-    if(d.shadowRoot) {
+  webComponents.forEach((d) => {
+    if (d.shadowRoot) {
       preventEnterOnIME(d.shadowRoot, signal)
     }
-  });
+  })
 
-  const input = Array.from(dom.querySelectorAll('input'));
-  const textarea = Array.from(dom.querySelectorAll('textarea'));
-
-  [...input, ...textarea].forEach((d) => {
-    d.addEventListener('keydown', (e: KeyboardEvent) => {
-      if(e.key == 'Enter') {
+  dom.addEventListener(
+    "keydown",
+    (e: KeyboardEvent) => {
+      if (!(e.target instanceof HTMLTextAreaElement || e.target instanceof HTMLInputElement)) return
+      if (e.key === "Enter") {
         if (e.isComposing) {
-          e.preventDefault();
-          e.stopPropagation();
-          lastKeyDownIsIME = true;
+          e.preventDefault()
+          e.stopPropagation()
+          lastKeyDownIsIME = true
         } else {
-          lastKeyDownIsIME = false;
+          lastKeyDownIsIME = false
         }
       }
-    }, {
+    },
+    {
       capture: true,
       signal
-    })
+    }
+  )
 
-    d.addEventListener('keyup', (e: KeyboardEvent) => {
-      if(e.key == 'Enter' && lastKeyDownIsIME) {
-        e.preventDefault();
-        e.stopPropagation();
-        lastKeyDownIsIME = false;
+  dom.addEventListener(
+    "keyup",
+    (e: KeyboardEvent) => {
+      if (!(e.target instanceof HTMLTextAreaElement || e.target instanceof HTMLInputElement)) return
+      if (e.key === "Enter" && lastKeyDownIsIME) {
+        e.preventDefault()
+        e.stopPropagation()
+        lastKeyDownIsIME = false
       }
-    }, {
+    },
+    {
       capture: true,
       signal
-    })
-  })
+    }
+  )
 }
